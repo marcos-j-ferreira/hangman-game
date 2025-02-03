9bc1c3f2cb3634c8d672b4d912c8d100d1b1adf3
marcos-j-ferreira <marcos.j.lemes.2004@gmail.com>
2025-02-03 20:34:04 -0300

    70% pronto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random 
 
-words: list = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon", "mango", "nectarine", "orange", "papaya", "quince", "raspberry", "strawberry", "tangerine", "ugli", "vanilla", "watermelon", "ximenia", "yuzu", "zucchini"]
+words: list = ["casa", "torre", "marcos"]
 
 def sort (words:list = [None]) -> list: # yes
     return random.choice(words)
@@ -10,17 +10,18 @@ def __a (word:str = None) -> list:
     #word = sort(words)
 
     arr:list  = []
-    size:int = len(word)
+    size:int = len(word) 
 
     for i in range(size):
         arr.append("_")
+
     return arr
 
-def _v(letter:str = None):
+def _v(word, letter:str = None):
 
-    word:str = sort(words)
+    #word:str = sort(words)
 
-    new_w = __a(word)
+   #new_w = __a(word)
 
 
     if letter is None:
@@ -38,43 +39,83 @@ def _v(letter:str = None):
         
     return False
 
-def main():
 
-    life:int = 4
-
-    word = sort(words)
-    new_w = __a(word)
-    nao:list = []
-
-
-    while life > 0:
-
-        letter:str = str(input("Enter a letter: "))
-
-        validacao = _v(letter)
-
-        if validacao is True:
-
-            for i in range(len(word)):
-                if letter == word[i]:
-                    new_w[i] = letter
-                    nao.append(letter)
-            
-
-        if validacao is False:
-            life -= 1
-            nao.append(letter)
-
-        __print(word, letter, new_w, life, nao)
-    
-    print( " Fim de Jgo! \n")
-
-def __print(word, letter, new_w, life, nao):
+def __print(letter, new_w, life, nao):
 
     ope:str = f"\n\n Vidas: {life}\n letras digitadas: {nao}\n letra digitada: {letter}\n\n Palavra: {new_w}\n\n"
     print(" \n------- Jogo da forca ------- \n")
     print(ope)
 
+def v_word(letter:str, nao:list = [None]) -> bool:
 
+    size:int = len(nao)
+
+    for i in range(size):
+        if letter == nao[i]:
+            return True
+    
+    return False
+
+def main():
+
+    life:int = 4
+    word = sort(words)
+    new_w = __a(word)
+  
+    # word = sort(words)
+    # new_w = __a(word)
+    # nao:list = []
+
+    nao:list = []
+
+    while life > 0:
+
+        letter:str = str(input("Enter a letter: "))
+
+        validacao = _v(word, letter)
+
+        b:bool = v_word(letter, nao)
+
+        if b is True:
+            print("Letra já digitada")
+            continue
+        nao.append(letter)
+
+        if validacao is True:
+
+            #add(letter, word, new_w, nao, life)
+            #b:bool = v_word(letter, nao)
+            #nao.append(letter)
+
+            size:int = len(new_w) 
+
+            for i in range(size):
+                
+                if  word[i] == letter:
+                    new_w[i] = letter
+            
+        if validacao is False:
+            life -= 1
+            #nao.append(letter)
+
+
+        __print( letter,new_w, life, nao)
+    
+    print( " Fim de Jgo! \n")
+
+# def add(word, letter,new_w, nao, life):
+
+#     lifee = life
+
+
+#     for i in range(len(word)):
+#         if letter == word[i]:
+#             new_w[i] = letter
+#             nao.append(letter)
+            
+
+#     __print(letter, new_w, lifee, nao)
+
+    
 main()
 
